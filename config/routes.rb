560b302fe9078624cf78bd880f7c91f49776b5c8
Ariana Sherman <ariana.sherman@kellogg.northwestern.edu>
2025-02-23 19:54:19 +0000

    redirected /entries to go to places list

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resources "places"
   get("/places", {:controller => "places", :action => "index"})
   get("/places/:id", {:controller=>"places", :action=>"show"})
+  get("/entries", { :controller => "places", :action => "index" })
   resources "entries"
-
+  
 end
